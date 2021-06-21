2ecd2a531e0d66a7129197a34354f73fab653a45
Evgeny Mufteev <evgenymufteev@gmail.com>
2021-06-21 14:07:35 +0300

    Столкнулся с проблемой подключения js файлов

--- app/JS/app.js
+++ app/JS/app.js
@@ -38,103 +38,101 @@ autoplayTimeout: 3000,
 });
 
 // popup
-const popupLinks = document.querySelectorAll('.popup-link');
-const body = document.querySelector('body');
-const loskPadding = document.querySelectorAll(".losk-padding");
 
-let unlock = true;
-
-const timeout = 800;
-
-if (popupLinks.length > 0) {
-for (let index = 0; index < popupLinks.length; index++) {
-  const popupLink = popupLinks[index];
-  popupLink.addEventListener("click", function (e) {
-    const popupName = popupLink.getAttribute('href').replace('#', '');
-    const curentPopup = document.getElementById(popupName);
-    popupOpen(curentPopup);
-    e.preventDefault();
-  });
-}
-}
+!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
 
 
-const popupCloseIcon = document.querySelectorAll('.close-popup');
+document.addEventListener('DOMContentLoaded', function() {
 
-if (popupCloseIcon.length > 0) {
-for (let index = 0; index < popupCloseIcon.length; index++) {
-  const el = popupCloseIcon[index];
-  el.addEventListener('click', function (e) {
-    popupCloseIcon(el.closest('.popup'));
-    e.preventDefault();
+   /* Записываем в переменные массив элементов-кнопок и подложку.
+      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+   var modalButtons = document.querySelectorAll('.js-open-modal'),
+       overlay      = document.querySelector('.js-overlay-modal'),
+       closeButtons = document.querySelectorAll('.js-modal-close');
+
+
+   /* Перебираем массив кнопок */
+   modalButtons.forEach(function(item){
+
+      /* Назначаем каждой кнопке обработчик клика */
+      item.addEventListener('click', function(e) {
+
+         /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+            Нужно подстраховаться. */
+         e.preventDefault();
+
+         /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+            и будем искать модальное окно с таким же атрибутом. */
+         var modalId = this.getAttribute('data-modal'),
+             modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+
+
+         /* После того как нашли нужное модальное окно, добавим классы
+            подложке и окну чтобы показать их. */
+         modalElem.classList.add('active');
+         overlay.classList.add('active');
+      }); // end click
+
+   }); // end foreach
+
+
+   closeButtons.forEach(function(item){
+
+      item.addEventListener('click', function(e) {
+         var parentModal = this.closest('.modal');
+
+         parentModal.classList.remove('active');
+         overlay.classList.remove('active');
+      });
+
+   }); // end foreach
+
+
+    document.body.addEventListener('keyup', function (e) {
+        var key = e.keyCode;
+
+        if (key == 27) {
+
+            document.querySelector('.modal.active').classList.remove('active');
+            document.querySelector('.overlay').classList.remove('active');
+        };
+    }, false);
+
+
+    overlay.addEventListener('click', function() {
+        document.querySelector('.modal.active').classList.remove('active');
+        this.classList.remove('active');
     });
-  }
-}
 
-function popupOpen(curentPopup) {
-if (curentPopup && unlock) {
-  const popupActive = document.querySelector('.popup.open');
-  if (popupActive) {
-    popupClose(popupActive, false);
-  } else  {
-    // bodyLock();
-  }
-  curentPopup.classList.add('open');
-  curentPopup.addEventListener("click", function (e) {
-    if (!e.target.closest('.popup__content')) {
-        popupClose(e.target.closest('.popup'));
-    }
-  })
-}
-}
 
-function popupClose(popupActive, doUnlock = true) {
-if (unlock) {
-  popupActive.classList.remove('open');
-  if (doUnlock) {
-    bodyUnLock();
-    }
-  }
-}
-
-function bodyLock() {
-const lockPaddingValue = window.innerWidth - document.querySelector('.page').offsetWidth + 'px';
-
-if (lockPadding.length > 0) {
-  for (let index = 0; index < loskPadding.length; index++) {
-    const el = loskPadding[index];
-    el.style.paddingRight = lockPaddingValue;
-  }
-}
-
-body.style.paddingRight = lockPaddingValue;
-body.classList.add('lock'); 
-
-unlock = false;
-setTimeout(function () {
-  unlock = true;
-}, timeout);
-}
-
-function bodyUnLock() {
-  setTimeout(function () {
-    if (loskPadding.length > 0) {
-      for (let index = 0; index < lockPadding.length; index++) {
-        const el = lockPadding[index];
-        el.style.paddingRight = '0px';
-      }
-    }
-    body.style.paddingRight = '0px';
-    body.classList.remove('lock');
-  }, timeout);
-
-  unlock = false;
-  setTimeout(function () {
-    unlock = true;
-  }, timeout);
-}
+}); // end ready
 //END popup
 
 
 
 
+// Слайдер в карточке товара
+
+    var swiper = new Swiper(".goodsSwiper", {
+      loop: true,
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".goodsSwiper2", {
+      loop: true,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      thumbs: {
+        swiper: swiper,
+      },
+    });
+
+
+  // END Слайдер в карточке товара
