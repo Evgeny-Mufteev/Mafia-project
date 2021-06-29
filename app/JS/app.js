13f563643c2de8645870a0839c2562a3982dfcc8
Evgeny Mufteev <EvgenyMufteev@gmai.com>
2021-06-29 23:20:35 +0300

    сделал адаптив шапки и подвала,так же весь адаптив главной страницы

--- app/JS/app.js
+++ app/JS/app.js
@@ -275,3 +275,18 @@ document.addEventListener('DOMContentLoaded', function() {
     })
   }
   checkCookies();
+
+
+
+
+
+let x = document.getElementById("contain");
+
+x.addEventListener("click", myFunction);
+
+function myFunction() {
+  var element = document.getElementById("nav");
+  element.classList.toggle("open");
+
+  x.classList.toggle("change");
+}
